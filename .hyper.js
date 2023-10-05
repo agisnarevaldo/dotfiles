fc237092f1b7891d2b0adfcaeef60e9b630caa38
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-10-05 23:33:26 +0700

    README.md

--- .hyper.js
+++ .hyper.js
@@ -8,7 +8,7 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 12,
+        fontSize: 13,
         // font family with optional fallbacks
         fontFamily: '"Hack Nerd Font", "JetbrainsMono Nerd Font", Menlo, monospace',
         // default font weight: 'normal' or 'bold'
@@ -138,7 +138,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyperpower", "hyper-night-owl"],
+    plugins: ["hyperpower"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
